fd49a44b4b5ee5461ab57059ffa16491d9ba5a80
competitveblood <shivangimasterblaster@gmail.com>
2024-09-12 17:12:45 +0530

    git commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">
+  return (<h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">
   Hello world!
 </h1>
+  );
 }
 
-export default App
+export default App;
