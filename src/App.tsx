bfb43880834579bc16f43ef33ec97de20adf536c
competitveblood <shivangimasterblaster@gmail.com>
2024-09-14 04:05:53 +0530

    yo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 function App() {
-  return (<h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">
+  return (
+  <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">
   Hello world!
 </h1>
   );
